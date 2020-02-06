9c7190beec232b78210ecd07f858867adbdb209d
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-02-06 14:07:37 -0800

    hide key

--- popcollector/settings.py
+++ popcollector/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secrets import secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^iq)igvlmj#%p%!m8f61u-pv6-!wl^p@m%q#mfth+=+v6oo#h)'
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
